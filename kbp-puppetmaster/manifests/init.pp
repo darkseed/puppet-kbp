bb28327c18671214c7f9f87dc404900f122a09f7
Bart Cortooms <bart@kumina.nl>
2008-11-05 17:56:04 +0100

    Add support for both Nginx as well as Apache.
    darcs-hash:20081105165604-431e1-439ef5718eecd5d7ca1a553e84f808156114a090.gz

--- kbp-puppetmaster/manifests/init.pp
+++ kbp-puppetmaster/manifests/init.pp
@@ -1,49 +1,58 @@
 class kbp-puppetmaster {
-	include apt
-
-	package { "mongrel":
-		ensure => present,
-		require => Apt::Source["etch-backports"],
-	}
-
-	package { "puppetmaster":
-		ensure => present,
-	}
-
-	service { "puppetmaster":
-		ensure => running,
-		enable => true,
-		require => Package["puppetmaster"],
-		subscribe => File["/etc/default/puppetmaster"],
-	}
-
-	file { "/etc/default/puppetmaster":
-		source => "puppet://puppet/kbp-puppetmaster/default/puppetmaster",
-		owner => "root",
-		group => "root",
-		mode => 644,
-		require => Package["puppetmaster"],
-	}
-
-	nginx::site_config { "puppet.$domain":
-		source => "puppet://puppet/kbp-puppetmaster/nginx/sites-available/puppet.$domain",
-	}
-
-	nginx::site { "puppet.$domain":
-		ensure => present,
-	}
-
-	# Needed for the Mongrel package
-	apt::source { "etch-backports":
-		comment => "Repository for packages which have been backported to Etch.",
-		sourcetype => "deb",
-		uri => "http://apt-proxy:9999/backports/",
-		distribution => "etch-backports",
-		components => "main",
-		require => Apt::Key["16BA136C"];
-	}
-
-        apt::key { "16BA136C":
+        package { ["puppetmaster", "mongrel", "rails"]:
                 ensure => present,
+        }
+
+        service { "puppetmaster":
+                ensure => running,
+                enable => true,
+                require => Package["puppetmaster"],
+                subscribe => File["/etc/default/puppetmaster"],
+        }
+
+        file { "/etc/default/puppetmaster":
+                source => "puppet://puppet/kbp-puppetmaster/default/puppetmaster",
+                owner => "root",
+                group => "root",
+                mode => 644,
+                require => Package["puppetmaster"],
+        }
+
+        if tagged(nginx) {
+                nginx::site_config {
+                        "puppet.$domain":
+                                template => "kbp-puppetmaster/nginx/puppetmaster.conf";
+                        "puppetca.$domain":
+                                template => "kbp-puppetmaster/nginx/puppetca.conf";
+                }
+
+                nginx::site {
+                        "puppet.$domain":
+                                ensure => present;
+                        "puppetca.$domain":
+                                ensure => present;
+                }
+        }
+
+        if tagged(apache) {
+                # Needed for Mongrel
+                apache::module { ["ssl", "proxy", "proxy_http", "proxy_balancer", "headers"]:
+                        ensure => present,
+                }
+
+                apache::site_config { "puppet.$domain":
+                        template => "kbp-puppetmaster/apache2/puppetmaster.conf",
+                }
+
+                apache::site { "puppet.$domain":
+                        ensure => present,
+                }
+        }
+
+        file { "/etc/munin/plugins/puppetmasterd_process_memory":
+                ensure => link,
+                target => "/usr/local/share/munin/plugins/puppetmasterd_process_memory",
+                require => File["/usr/local/share/munin/plugins"],
+                notify => Service["munin-node"],
         }
 }
