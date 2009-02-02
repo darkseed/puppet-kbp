68604761a63ee7d0a967ddf61e5c7bed029d0a0a
Bart Cortooms <bart@kumina.nl>
2009-02-02 14:53:00 +0100

    The send_nsca configuration file belongs in kbp-munin as well.
    darcs-hash:20090202135300-431e1-f555f2b6c78d8fb7efba85c7b8db2aa10cd67b25.gz

--- kbp-munin/manifests/init.pp
+++ kbp-munin/manifests/init.pp
@@ -3,4 +3,12 @@ class kbp-munin::client inherits munin::client {
 
 class kbp-munin::server inherits munin::server {
 	include nagios::nsca
+
+	file { "/etc/send_nsca.cfg":
+		source => "puppet://puppet/kbp-munin/server/send_nsca.cfg",
+		mode => 640,
+		owner => "root",
+		group => "munin",
+		require => Package["nsca"],
+	}
 }
