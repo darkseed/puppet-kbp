39854ab1c32ae1c792ad0ac873bdacd6219f5c1d
Bart Cortooms <bart@kumina.nl>
2009-02-17 16:38:14 +0100

    Add kbp-syslog module, which does the right thing for Etch and Lenny.
    darcs-hash:20090217153814-431e1-ce10e70dc8dcbddad70e43d2a67313cde1c3d515.gz

---
+++ kbp-syslog/manifests/init.pp
@@ -0,0 +1,31 @@
+class kbp-syslog::server {
+	include "kbp-syslog::server::$lsbdistcodename"
+}
+
+class kbp-syslog::client {
+	include "kbp-syslog::client::$lsbdistcodename"
+}
+
+class kbp-syslog::server::etch inherits syslog-ng::server {
+	file { "/etc/logrotate.d/syslog-ng":
+		source => "puppet://puppet/kbp-syslog/server/logrotate.d/syslog-ng",
+		owner => "root",
+		group => "root",
+		mode => 644,
+	}
+}
+
+class kbp-syslog::client::etch inherits sysklogd::client {
+}
+
+class kbp-syslog::server::lenny inherits rsyslog::server {
+	file { "/etc/logrotate.d/rsyslog":
+		source => "puppet://puppet/kbp-syslog/server/logrotate.d/rsyslog",
+		owner => "root",
+		group => "root",
+		mode => 644,
+	}
+}
+
+class kbp-syslog::client::lenny inherits rsyslog::client {
+}
