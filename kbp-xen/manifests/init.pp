5dd83adad9740ab1b50302c964fceab3150f33bd
Kees Meijs <kees@kumina.nl>
2009-04-20 16:34:40 +0200

    Puppify xen-tools.conf
    darcs-hash:20090420143440-6c6a4-f8509b45a487b134cbbff0ed1000f6fa8ab8d4ba.gz

--- kbp-xen/manifests/init.pp
+++ kbp-xen/manifests/init.pp
@@ -1,4 +1,36 @@
 class kbp-xen::dom0 inherits xen::dom0 {
+	include "kbp-xen::dom0::$lsbdistcodename"
+
+	file {
+		"/etc/xen-tools/role.d/kumina":
+			source => "puppet://puppet/kbp-xen/xen-tools/role.d/kumina",
+			owner => "root",
+			group => "root",
+			mode => 755,
+			require => Package["xen-tools"];
+	}
+}
+
+class kbp-xen::dom0::etch {
+	file {
+		"/etc/xen-tools/xen-tools.conf":
+			source => "puppet://puppet/kbp-xen/xen-tools/xen-tools.conf-etch",
+			owner => "root",
+			group => "root",
+			mode => 644,
+			require => Package["xen-tools"];
+	}
+}
+
+class kbp-xen::dom0::lenny {
+	file {
+		"/etc/xen-tools/xen-tools.conf":
+			source => "puppet://puppet/kbp-xen/xen-tools/xen-tools.conf-lenny",
+			owner => "root",
+			group => "root",
+			mode => 644,
+			require => Package["xen-tools"];
+	}
 }
 
 class kbp-xen::domu inherits xen::domu {
