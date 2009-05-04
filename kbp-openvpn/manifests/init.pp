178cfa71caf4f50c12c9e0b1f333c69be0381fff
Bart Cortooms <bart@kumina.nl>
2009-05-04 21:01:16 +0200

    The Munin plugin for OpenVPN has hardcoded the location of the OpenVPN status log, so a symlink is needed.
    darcs-hash:20090504190116-431e1-7667842e67c79cc1c5da748c33559401669d3e6c.gz

--- kbp-openvpn/manifests/init.pp
+++ kbp-openvpn/manifests/init.pp
@@ -1,9 +1,25 @@
 class kbp-openvpn::server inherits openvpn::server {
-	file { "/etc/munin/plugins/openvpn":
+	file {
+		"/etc/munin/plugins/openvpn":
+			ensure => link,
+			owner => "root",
+			group => "root",
+			target => "/usr/share/munin/plugins/openvpn",
+			require => [File["/etc/openvpn/openvpn-status.log"],
+			            File["/etc/munin/plugin-conf.d/openvpn"]],
+			notify => Service["munin-node"];
+		"/etc/munin/plugin-conf.d/openvpn":
+			owner => "root",
+			group => "root",
+			mode => 644,
+			content => "[openvpn]\nuser root\n",
+			notify => Service["munin-node"];
+	}
+
+	# The Munin plugin has hardcoded the location of the status log, so we
+	# need this symlink.
+	file { "/etc/openvpn/openvpn-status.log":
 		ensure => link,
-		owner => "root",
-		group => "root",
-		target => "/usr/share/munin/plugins/openvpn",
-		notify => Service["munin-node"],
+		target => "/var/lib/openvpn/status.log",
 	}
 }
