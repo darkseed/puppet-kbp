3298cda672b703a6b029fdef68eca89afcbabb75
Bart Cortooms <bart@kumina.nl>
2009-05-08 13:44:34 +0200

    Move setup for memory backed /var/lib/munin to kbp-munin.
    darcs-hash:20090508114434-431e1-82c225362b566d6fe4aeba66c469c54ddb257d47.gz

--- kbp-munin/manifests/init.pp
+++ kbp-munin/manifests/init.pp
@@ -11,4 +11,31 @@ class kbp-munin::server inherits munin::server {
 		group => "munin",
 		require => Package["nsca"],
 	}
+
+	package { "rsync":
+		ensure => installed,
+	}
+
+	# The RRD files for Munin are stored on a memory backed filesystem, so
+	# sync it to disk on reboots.
+	file { "/etc/init.d/munin-server":
+		source => "puppet://puppet/munin/server/init.d/munin-server",
+		mode => 755,
+		owner => "root",
+		group => "root",
+		require => [Package["rsync"], Package["munin"]],
+	}
+
+	service { "munin-server":
+		enable => true,
+		require => File["/etc/init.d/munin-server"],
+	}
+
+	# Cron job which syncs the RRD files to disk every 30 minutes.
+	file { "/etc/cron.d/munin-sync":
+		source => "puppet://puppet/munin/server/cron.d/munin-sync",
+		owner => "root",
+		group => "root",
+		require => [Package["munin"], Package["rsync"]];
+	}
 }
