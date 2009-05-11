5e61f4fa0e754ae916ac08b2eff7fdd49065113d
Bart Cortooms <bart@kumina.nl>
2009-05-11 12:52:45 +0200

    Add munin.conf which makes use of memory backed Munin data files.
    darcs-hash:20090511105245-431e1-96ea16028f0ce38d10f276070ed0ec8a7b992234.gz

--- kbp-munin/manifests/init.pp
+++ kbp-munin/manifests/init.pp
@@ -7,6 +7,10 @@ class kbp-munin::client inherits munin::client {
 class kbp-munin::server inherits munin::server {
 	include nagios::nsca
 
+	File["/etc/munin/munin.conf"] {
+		source => "puppet://puppet/kbp-munin/server/munin.conf",
+	}
+
 	file { "/etc/send_nsca.cfg":
 		source => "puppet://puppet/kbp-munin/server/send_nsca.cfg",
 		mode => 640,
