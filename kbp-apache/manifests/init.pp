0a55984717f9a54637ac0474b36417ab0bcdbec2
Bart Cortooms <bart@kumina.nl>
2009-05-26 17:03:23 +0200

    kbp-apache module, which adds configuration for hardening and enables the mod_deflate module.
    darcs-hash:20090526150323-431e1-fe730063e0a668c7ca044866d70cb722e080c017.gz

---
+++ kbp-apache/manifests/init.pp
@@ -0,0 +1,22 @@
+class kbp-apache inherits apache {
+	file {
+		"/etc/apache2/mods-available/deflate.conf":
+			source => "puppet://puppet/kbp-apache/mods-available/deflate.conf",
+			owner => "root",
+			group => "root",
+			mode => 644,
+			require => Package["apache2"],
+			notify => Exec["reload-apache2"];
+		"/etc/apache2/conf.d/security":
+			source => "puppet://puppet/kbp-apache/conf.d/security",
+			owner => "root",
+			group => "root",
+			mode => 644,
+			require => Package["apache2"],
+			notify => Exec["reload-apache2"];
+	}
+
+	apache::module { "deflate":
+		ensure => present,
+	}
+}
