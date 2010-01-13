9f86127df3496e2e9d42ba495bf5487b3148203d
Kees Meijs <kees@kumina.nl>
2010-01-13 15:37:43 +0100

    Defaults for kbp-libvirt
    darcs-hash:20100113143743-6c6a4-12e9d1761084efddb0073d94d52aafc7912e6fcf.gz

--- kbp-libvirt/manifests/init.pp
+++ kbp-libvirt/manifests/init.pp
@@ -9,6 +9,33 @@ class kbp-libvirt inherits libvirt {
 			ensure => installed;
 	}
 
+	file {
+		"/etc/libvirt/qemu/networks/default.xml":
+			require => Package["libvirt-bin"],
+			ensure => absent;
+		"/etc/libvirt/storage":
+			ensure => directory,
+			require => Package["libvirt-bin"],
+			owner => "root",
+			group => "root",
+			mode => 755;
+		"/etc/libvirt/storage/autostart":
+			ensure => directory,
+			require => File["/etc/libvirt/storage"],
+			owner => "root",
+			group => "root",
+			mode => 755;
+		"/etc/libvirt/storage/guest.xml":
+			source => "puppet://puppet/kbp-libvirt/libvirt/storage/guest.xml",
+			require => File["/etc/libvirt/storage"],
+			owner => "root",
+			group => "root",
+			mode => 644;
+		"/etc/libvirt/storage/autostart/guest.xml":
+			ensure => "/etc/libvirt/storage/guest.xml",
+			require => File["/etc/libvirt/storage/autostart"];
+	}
+
 	munin::client::plugin {
 		["libvirt-blkstat", "libvirt-cputime", "libvirt-ifstat", "libvirt-mem"]:
 			require => Package["python-libvirt", "python-libxml2"];
