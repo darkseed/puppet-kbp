04b43ac150553d862f655974340d2d9357167e2e
Kees Meijs <kees@kumina.nl>
2010-01-13 14:48:25 +0100

    Clean up kbp-libvirt module
    darcs-hash:20100113134825-6c6a4-b0b097b60db821bc4cccd5936692edf33fb3c295.gz

--- kbp-libvirt/manifests/init.pp
+++ kbp-libvirt/manifests/init.pp
@@ -1,25 +1,16 @@
 class kbp-libvirt inherits libvirt {
 	include munin::client
 
-	File["/etc/libvirt/libvirtd.conf"] {
-		source => undef,
-		content => template("kbp-libvirt/libvirt/libvirtd.conf"),
-	}
-
-	if ($lsbdistrelease == "4.0") {
-		include debian::backports
-
-		Package["libvirt-bin"] {
-			require +> Apt::Source["etch-backports"],
-		}
-	}
-
 	# Munin plugins
-	package { ["python-libxml2", "python-libvirt"]:
-		ensure => installed,
+	package {
+		"python-libxml2":
+			ensure => installed;
+		"python-libvirt":
+			ensure => installed;
 	}
 
-	munin::client::plugin { ["libvirt-blkstat", "libvirt-cputime", "libvirt-ifstat", "libvirt-mem"]:
-		require => [Package["python-libvirt"], Package["python-libxml2"]],
+	munin::client::plugin {
+		["libvirt-blkstat", "libvirt-cputime", "libvirt-ifstat", "libvirt-mem"]:
+			require => Package["python-libvirt", "python-libxml2"];
 	}
 }
