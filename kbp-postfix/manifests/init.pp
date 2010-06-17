9fec5cc3238eed1decf15297a009fea2f77c8ec4
Kees Meijs <kees@kumina.nl>
2010-06-17 16:01:10 +0200

    Restart Postfix when /etc/ssl/certs changes
    darcs-hash:20100617140110-6c6a4-2ec718f52642ca8d085909c5e01351665c8a3ebe.gz

--- kbp-postfix/manifests/init.pp
+++ kbp-postfix/manifests/init.pp
@@ -1,7 +1,14 @@
 class kbp-postfix inherits postfix {
 	include munin::client
+	include openssl::common
 
         munin::client::plugin { ["postfix_mailqueue", "postfix_mailstats", "postfix_mailvolume"]:
 		ensure => present,
+	}
+
+	# The Postfix init script copies /etc/ssl/certs stuff on (re)start, so restart Postfix
+	# on changes!
+	Service["postfix"] {
+		subscribe => File["/etc/ssl/certs"],
 	}
 }
