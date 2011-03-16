89369055ec71af117e0f60bbca5935fff40f347b
Ed Schouten <ed@kumina.nl>
2011-03-16 16:34:00 +0100

    Voeg Pacemaker-module toe.
    darcs-hash:20110316153400-14095-a5203036b70fb1741a51acca45cd79e713455583.gz

---
+++ kbp-pacemaker/manifests/init.pp
@@ -0,0 +1,8 @@
+# Copyright (C) 2010 Kumina bv, Ed Schouten <ed@kumina.nl>
+# This works is published under the Creative Commons Attribution-Share 
+# Alike 3.0 Unported license - http://creativecommons.org/licenses/by-sa/3.0/
+# See LICENSE for the full legal text.
+
+class kbp-pacemaker {
+	include pacemaker
+}
