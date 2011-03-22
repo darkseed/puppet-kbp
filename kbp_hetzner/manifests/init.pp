f12e69e52099f3e55de79cfb835f300d405e96e5
Pieter Lexis <pieter@kumina.nl>
2011-03-22 11:51:09 +0100

    Hetzner module verbeterd
    darcs-hash:20110322105109-a572e-25adb8a4403c735812d04b860631f0cb9fb47f5c.gz

--- kbp_hetzner/manifests/init.pp
+++ kbp_hetzner/manifests/init.pp
@@ -1,6 +1,5 @@
 class kbp_hetzner inherits hetzner {
 	include munin::client
-
 	package { "lm-sensors":
 		ensure => "latest";
 	}
@@ -8,17 +7,23 @@ class kbp_hetzner inherits hetzner {
 	munin::client::plugin { "sensors_temp":
 		require => Package["lm-sensors"],
 		script  => "sensors_",
-	}
-
-	exec { "/sbin/modprobe f71882fg":
-		
-	}
-
-	file { "/etc/modprobe.d/hetzner-sensors.conf":
-		content => "f71882fg",
-		ensure  => "present",
 		notify  => Exec["/sbin/modprobe f71882fg"];
 	}
 
+	exec { "/sbin/modprobe f71882fg":
+		refreshonly => true,
+	}
 
+	#can be removed after all hosts have kickpuppet'd
+	file { "/etc/modprobe.d/hetzner-sensors.conf":
+		content => "f71882fg\n",
+		ensure  => "absent",
+		notify  => Exec["/sbin/modprobe f71882fg"];
+	}
+
+	line { "f71882fg": # Load the module on boot
+		file 	=> "/etc/modules",
+		ensure 	=> "present",
+		content	=> "f71882fg";
+	}
 }
